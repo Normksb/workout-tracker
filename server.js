4e6d5a870ad72b345a08d2860a410c0628a4862f
Norman Bernard <nksb414@gmail.com>
2021-10-27 15:34:01 +0800

    created server.js and added first route to serve index.html

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const db = require('./models');
+const path = require("path");
+const htmlRoutes = require("./controller/html-routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log("Now listening on port", PORT);
+  });
